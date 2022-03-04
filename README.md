e95b7308feabdb7e2186e8800420eb22ac2e381d
sosow0212 <sosow0212@naver.com>
2022-03-04 16:35:00 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+### Spring boot
+#### RestApi self Studying
+
+- Spring web
+- Lombok
+- Thymeleaf
+- Jpa
+- MariaDB
---
+++ src/main/java/self/study/SpringSelfStudyApplication.java
@@ -0,0 +1,13 @@
+package self.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSelfStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSelfStudyApplication.class, args);
+    }
+
+}
