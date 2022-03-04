ee76ef2235af6d549e40cb68f03f82866fc97c76
sosow0212 <sosow0212@naver.com>
2022-03-04 17:12:09 +0900

    GlobalExceptionHandler

---
+++ src/main/java/self/study/handler/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package self.study.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@ControllerAdvice
+@RestController
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    private ResponseEntity<?> handleArgumentException(Exception e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
