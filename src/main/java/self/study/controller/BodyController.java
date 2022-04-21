56b75bc5535028a905ef10e678e41b3c8587533f
sosow0212 <sosow0212@naver.com>
2022-04-21 20:39:46 +0900

    Body info - CRUD 완성 / 테스트 X

--- src/main/java/self/study/controller/BodyController.java
+++ src/main/java/self/study/controller/BodyController.java
@@ -3,8 +3,8 @@ package self.study.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import self.study.entity.Body;
 import self.study.service.BodyService;
 
 @RequiredArgsConstructor
@@ -14,7 +14,28 @@ public class BodyController {
     private final BodyService bodyService;
 
     @GetMapping("/api/body")
-    public ResponseEntity<?> loadUsersBodyInfo() {
-        return new ResponseEntity<>(bodyService.loadUsersBodyInfo(), HttpStatus.OK);
+    public ResponseEntity<?> loadAllBodyInfo() {
+        return new ResponseEntity<>(bodyService.loadAllBodyInfo(), HttpStatus.OK);
+    }
+
+    @GetMapping("/api/body/{id}")
+    public ResponseEntity<?> loadUserBodyInfo(@PathVariable("id") Integer id) {
+        return new ResponseEntity<>(bodyService.loadUserBodyInfo(id), HttpStatus.OK);
+    }
+
+    @PostMapping("/api/body/save")
+    public ResponseEntity<?> saveBodyInfo(@RequestBody Body body) {
+        return new ResponseEntity<>(bodyService.saveBodyInfo(body), HttpStatus.OK);
+    }
+
+    // 확인 필요
+    @PutMapping("/api/body/{id}/edit")
+    public ResponseEntity<?> editBodyInfo(@RequestBody Body newBody, @PathVariable("id") Integer id) {
+        return new ResponseEntity<>(bodyService.updateBody(id, newBody), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/api/body/{id}/delete")
+    public ResponseEntity<?> deleteBodyInfo(@PathVariable("id") Integer id) {
+        return new ResponseEntity<>(bodyService.deleteBodyInfo(id), HttpStatus.OK);
     }
 }
--- src/main/java/self/study/service/BodyService.java
+++ src/main/java/self/study/service/BodyService.java
@@ -2,6 +2,7 @@ package self.study.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import self.study.entity.Body;
 import self.study.repository.BodyRepository;
 
@@ -13,7 +14,40 @@ public class BodyService {
 
     private final BodyRepository bodyRepository;
 
-    public List<Body> loadUsersBodyInfo() {
+    @Transactional(readOnly = true)
+    public List<Body> loadAllBodyInfo() {
         return bodyRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Body loadUserBodyInfo(Integer id) {
+        return bodyRepository.findById(id).orElseThrow(()->{
+            return new IllegalArgumentException("ID를 찾을 수 없습니다.");
+        });
+    }
+
+    @Transactional
+    public Body saveBodyInfo(Body body) {
+        return bodyRepository.save(body);
+    }
+
+    @Transactional
+    public Body updateBody(Integer userId, Body newBody) {
+        Body body = bodyRepository.findById(userId).orElseThrow(() -> {
+           return new IllegalArgumentException("ID를 찾을 수 없습니다.");
+        });
+        body = newBody;
+        return body;
+    }
+
+
+    @Transactional
+    public String deleteBodyInfo(Integer id) {
+        Body body = bodyRepository.findById(id).orElseThrow(()->{
+            return new IllegalArgumentException("ID를 찾을 수 없습니다.");
+        });
+
+        bodyRepository.delete(body);
+        return "삭제성공";
     }
 }
