891546d2d84d9872ba2ae6dd5a425075b5da34d5
sosow0212 <sosow0212@naver.com>
2022-05-12 11:24:24 +0900

    del: 기존 프론트 코드 삭제 (REST API를 위해서)

--- src/main/resources/static/store.js
+++ src/main/resources/static/store.js
@@ -1,48 +1,48 @@
-let index = {
-    init: function () {
-        $("#btn-save").on("click", () => {
-            this.save();
-        })
-        console.log("JS 실행")
-    },
-
-    save: function () {
-        let data = {
-            title: $("#title").val(),
-            content: $("#content").val(),
-        }
-
-        $.ajax({
-            type: "POST",
-            url: "/board/upload",
-            data: JSON.stringify(data),
-            contentType: "application/json; charset=utf-8",
-            dataType: "json"
-        }).done(function (res) {
-            alert("저장완료");
-            location.href = "/";
-        }).fail(function (error) {
-            alert(JSON.stringify(error));
-        });
-    },
-}
-
-
-// /upload 가면 GET 요청 실행
-// 추후에 리액트와 함께 쓰면, 렌더링도 가능
-function request() {
-    fetch('http://localhost:8080/store/1', {
-        method: 'GET',
-    })
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-        });
-}
-request();
-
-
-
-index.init();
+// let index = {
+//     init: function () {
+//         $("#btn-save").on("click", () => {
+//             this.save();
+//         })
+//         console.log("JS 실행")
+//     },
+//
+//     save: function () {
+//         let data = {
+//             title: $("#title").val(),
+//             content: $("#content").val(),
+//         }
+//
+//         $.ajax({
+//             type: "POST",
+//             url: "/board/upload",
+//             data: JSON.stringify(data),
+//             contentType: "application/json; charset=utf-8",
+//             dataType: "json"
+//         }).done(function (res) {
+//             alert("저장완료");
+//             location.href = "/";
+//         }).fail(function (error) {
+//             alert(JSON.stringify(error));
+//         });
+//     },
+// }
+//
+//
+// // /upload 가면 GET 요청 실행
+// // 추후에 리액트와 함께 쓰면, 렌더링도 가능
+// function request() {
+//     fetch('http://localhost:8080/store/1', {
+//         method: 'GET',
+//     })
+//         .then(response => {
+//             return response.json();
+//         })
+//         .then(data => {
+//             console.log(data);
+//         });
+// }
+// request();
+//
+//
+//
+// index.init();
