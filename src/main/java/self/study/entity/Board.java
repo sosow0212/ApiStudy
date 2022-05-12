6594ab072a1792d9da817f1214b13bd763d18b1f
sosow0212 <sosow0212@naver.com>
2022-05-12 10:32:26 +0900

    fix: Board Entity CASCADE 속성 추가

--- src/main/java/self/study/entity/Board.java
+++ src/main/java/self/study/entity/Board.java
@@ -3,6 +3,8 @@ package self.study.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 
@@ -21,6 +23,7 @@ public class Board {
     @Column(nullable = false)
     private String content;
 
+    @OnDelete(action = OnDeleteAction.CASCADE)
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
